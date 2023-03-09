ec0b34ab473998c72c262fa5c95ee5a33a17b777
LULUcas0528 <natalirabessa@gmail.com>
2023-03-09 18:32:09 -0300

    correção

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 const express = require ('express')
-const routes = require ('./routes')
+const routes = require ('./routes');
+const { default: teste2 } = require('./teste');
 const app = express()
 
 app.use(express.json());
 app.use(routes)
-
+console.log(teste2);
 module.exports =app;
