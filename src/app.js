772a80da70cfc685eed84ab90af42959b89c128e
LULUcas0528 <natalirabessa@gmail.com>
2023-03-09 18:35:56 -0300

    testes

--- src/app.js
+++ src/app.js
@@ -5,5 +5,5 @@ const app = express()
 
 app.use(express.json());
 app.use(routes)
-console.log(teste2);
+
 module.exports =app;
--- src/teste.js
+++ src/teste.js
@@ -1,5 +1,4 @@
-function teste2(){
-    return "Olar"
-}
-
-export default teste2
+const a = 10;
+const b = "5";
+const c = a + b;
+console.log(c);
