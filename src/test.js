7217731c97868359133b94f0b906b9081abc45b5
LULUcas0528 <natalirabessa@gmail.com>
2023-03-23 16:40:09 -0300

    test 555

--- src/test.js
+++ src/test.js
@@ -6,12 +6,12 @@ function ehPar(numero) {
 
 describe('A função é Par', () => {
   it('deve retornar true para números pares', () => {
-    expect(ehPar(3)).toBe(true);
+    expect(ehPar(2)).toBe(true);
     expect(ehPar(4)).toBe(true);
     expect(ehPar(10)).toBe(true);
   });
   it('deve retornar false para números ímpares', () => {
-    expect(ehPar(2)).toBe(false);
+    expect(ehPar(1)).toBe(false);
     expect(ehPar(3)).toBe(false);
     expect(ehPar(11)).toBe(false);
   });
